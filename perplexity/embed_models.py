a6443660c5e536a9144f4761a0a90a7869e2ee8d
buenavista62 <sercan-ates@outlook.com>
2024-07-02 14:16:55 +0200

    bugfix

--- perplexity/embed_models.py
+++ perplexity/embed_models.py
@@ -5,5 +5,3 @@ models = {"all-MiniLM-L6-v2":"sentence-transformers/all-MiniLM-L6-v2",
     "all-distilroberta-v1":"sentence-transformers/all-distilroberta-v1", 
     "gtr-t5-large":"sentence-transformers/gtr-t5-large"}
 
-
-models.keys()
